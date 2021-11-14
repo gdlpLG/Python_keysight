d6f73505cdace65959453cf0aacee4e5811b9161
Cl3m47 <clement.mallet47@hotmail.fr>
2021-11-14 23:52:46 +0100

    Analyse de fichiers CSV

--- Code_de_test/PyXLL_test.py
+++ Code_de_test/PyXLL_test.py
@@ -3,15 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dir = ('C:/Users/Cl3ment/Dropbox/MACRO Excel/Macro Extraction CSV/AAA DIODE/')
-# df = pd.concat([pd.read_csv(f'C:/Users/Cl3ment/Dropbox/MACRO Excel/Macro Extraction CSV/AAA DIODE/{f}', encoding= 'unicode_escape') for f in os.listdir(dir) if f.endswith('.csv')])
-
-# df = pd.concat([pd.read_csv(dir+f, encoding= 'unicode_escape') for f in os.listdir(dir) if f.endswith('.csv')])
-# df = [pd.read_csv(dir+f, encoding= 'unicode_escape') for f in os.listdir(dir) if f.endswith('.csv')]
-# df = pd.concat(df)
 
 for f in os.listdir(dir):
     if f.endswith('.csv'):
         # utiliser le nom du fichier
+        name = os.path.splitext(f)[0]
 
         # lire le fichier
         df = pd.read_csv(dir + f, engine='python', delimiter=',', skiprows=7, skipfooter=2)
@@ -22,11 +18,15 @@ for f in os.listdir(dir):
         # afficher dans le graphe
         x = df.iloc[:, [0]]
         y = df.iloc[:, [1]]
-        plt.plot(x, y, linewidth=1)
+        plt.plot(x, y, label=name, linewidth=2)
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
     else:
         continue
 
+plt.xlim(x.min()[0], x.max()[0])
+plt.tight_layout()
 plt.show()
+
+# print()
 # df.info()
-# print(df)
 # df.to_csv('C:/Users/Cl3ment/Dropbox/MACRO Excel/Macro Extraction CSV/Result.csv')
