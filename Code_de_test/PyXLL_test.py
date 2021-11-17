4ba6b44a11a15a5b73b71d07bf01c3e403007701
cl.mallet <clement.mallet47@hotmail.fr>
2021-11-17 11:21:50 +0100

    Maj du 17 nov 2021

--- Code_de_test/PyXLL_test.py
+++ Code_de_test/PyXLL_test.py
@@ -1,37 +1,42 @@
 import os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 
 # dir = ('C:/Users/Cl3ment/Dropbox/MACRO Excel/Macro Extraction CSV/AAA DIODE/')
-# dir = ('C:/Users/cl.mallet/Dropbox/MACRO Excel/Macro Extraction CSV/AAA DIODE/')
-# dir = ('D:/APM40KA/Sans_combineur/')
-dir = ('C:/Users/Cl3ment/Dropbox/MACRO Excel/Macro Extraction CSV/Sans_combineur/')
+dir = ('D:/APM40KA/Sans_combineur/')
+
+marker = itertools.cycle(('x', 'P', 'd', 'o', '*','^','v','s','<','>'))
 
 for f in os.listdir(dir):
     if f.endswith('.csv'):
-        # utiliser le nom du fichier
+        # Utiliser le nom du fichier
         name = os.path.splitext(f)[0]
-        print(name)
 
-        # lire le fichier
+        # Lire le fichier
         df = pd.read_csv(dir + f, engine='python', delimiter=',', skiprows=7, skipfooter=2)
 
-        # mettre en forme le fichier
-
-        # afficher dans le graphe
+        # Convertion des données
         x = df.iloc[0:401, [0]]
         x = x.replace('.', ',')
         x = x.astype(float)
         y = df.iloc[0:401, [1]]
         y = y.replace('.', ',')
         y = y.astype(float)
-        plt.plot(x, y, label=name, linewidth=2)
-        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+
+        # Afficher dans le graphe
+        plt.title('APM40KA #4')      # Titre du graphique
+        plt.xlabel('Fréquence [Hz]')    # Titre axe X
+        plt.ylabel('Gain [dB]')     # Titre axe Y
+        plt.plot(x, y, label=name,marker = next(marker),markevery = 40, linewidth=1.5)     # Légende
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)    # Position de la légende
     else:
         continue
 
-plt.xlim(x.min()[0], x.max()[0])
-plt.tight_layout()
+plt.xlim(x.min()[0], x.max()[0])        # Limite de l'axe X
+plt.xticks(np.arange(x.min()[0], x.max()[0]+1, 500e6))      # Echelle de l'axe X
+plt.tight_layout()      # Echelle de la fenêtre
 plt.show()
 
 # print()
